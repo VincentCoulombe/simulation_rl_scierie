c575a8f9ebb695f751b63c3745c07e0cdf01668e
Vincent_Coulombe <vincent.coulombe.5@ulaval.ca>
2022-06-06 16:29:10 -0400

    update gym

--- Gym.py
+++ Gym.py
@@ -6,15 +6,13 @@ from gym import spaces
 from EnvSimpy import *
 
 
-
 class EnvGym(gym.Env) : 
     
-    def __init__(self, paramSimu: dict, nb_actions: int, state_len: int, state_min: float, state_max: float, **kwargs):
+    def __init__(self, paramSimu: dict, nb_actions: int, state_len: int, state_min: float, state_max: float, *args, **kwargs):
         
-        super().__init__(**kwargs)
+        super().__init__(*args, **kwargs)
         self.paramSimu = paramSimu
         self.action_space = spaces.Discrete(nb_actions)
-
         self.low = np.array([state_min for _ in range(state_len)], dtype=np.float32)
         self.high = np.array([state_max for _ in range(state_len)], dtype=np.float32)
         self.observation_space = spaces.Box(low=self.low, high=self.high, dtype=np.float32)
@@ -28,35 +26,27 @@ class EnvGym(gym.Env) :
     
     def _update_reward(self) -> float:
         
-        self.respect_demande = self.env.getRespectDemande() #Pas normalisé
-        return 0.5*self.respect_demande + 0.1*0. #Calculer ici
+        self.respect_demande = sum(x for x in self.env.getRespectDemande() if x > 0)
+        return 1.0*self.respect_demande + 0.1*0. #......
                 
     def reset(self) -> np.array: 
         
         self.env = EnvSimpy(self.paramSimu) # Nouvelle simulation simpy       
         self.done = False
         self.info = {}
-                        
+        self.indicateurs = []                
         return self._update_observation()    
     
     def step (self, action, log_inds: bool=False) -> tuple: 
 
-        # ----------------------------------------------------------------------------------
-        # 1) faire l'action et rouler simu
-        
-        # 2) calculer indicateurs (attributs)
-        
-        # 3) normaliser state (attributs)
-        
-        # 3) return done ou pas
-        # ----------------------------------------------------------------------------------    
         self.done = self.env.stepSimpy(action)
         
         reward = self._update_reward()
         state = self._update_observation()
         
-        # 4) logger les indicateurs pertinents        
-        # self.indicateurs.append([self.respect_demande])
+        # logger les indicateurs pertinents 
+        if log_inds:       
+            self.indicateurs.append([self.env.now, reward])
         
         return state, reward, self.done, self.info
     
@@ -65,10 +55,13 @@ class EnvGym(gym.Env) :
         state = self.reset()  
         done = False
         while not done: 
-            action = ChoixLoader(state)
+            action = ChoixLoader(self.env)
             state, _, done, _ = self.step(action)
             
-
-# sourcery skip: remove-redundant-if
-if __name__ == '__main__': 
-    pass
+    def evaluate_model(self, model):
+        obs = self.reset()
+        done = False
+        while not done:
+            action, _ = model.predict(obs)
+            obs, _, done, _ = self.step(action)
+        self.indicateurs.plot(x="time", y=["reward"])
