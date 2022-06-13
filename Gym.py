caffb970098520c8c733d72137ccbafff4e947e3
Vincent_Coulombe <vincent.coulombe.5@ulaval.ca>
2022-06-13 09:54:05 -0400

    test

--- Gym.py
+++ Gym.py
@@ -61,7 +61,7 @@ class EnvGym(gym.Env) :
         self.reward = respect_obj_qte_total+respect_obj_proportion
         
     def get_avg_reward(self) -> float:
-        return np.array(self.rewards)[:, 1].mean()
+        return np.array(self.rewards)[1].mean()
                 
     def reset(self, test:bool =False) -> np.array: 
         if test:
